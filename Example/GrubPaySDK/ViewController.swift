5f588bb4026310445c4dc72acfcfb44999d1bae5
Edward <edward.yuan@iotpay.ca>
2023-06-28 14:09:42 -0700

    optimization

--- Example/GrubPaySDK/ViewController.swift
+++ Example/GrubPaySDK/ViewController.swift
@@ -14,9 +14,9 @@ import CommonCrypto
 class ViewController: UIViewController {
     // MARK: - Config For Testing
 
-    let mchId = "10001321"
-    let loginName = "eway"
-    let mchKey = "LkFe0lpxjFUU27xh9hXhPxU2yztzIcgv"
+    let mchId = "your_mchid"
+    let loginName = "your_username"
+    let mchKey = "your_merchant_key"
     let serverUrl = "https://api.grubpay.io/v4/"
 
     // MARK: - Properties
